f57e4b6b8ce13d82d186ce89f605d78729faffe8
reysilvaa <reynaldsilva123@gmail.com>
2025-04-08 14:47:20 +0700

    Update CORS configuration in CustomIoAdapter to allow all origins, simplifying access for clients. Removed specific origins and some allowed headers to streamline the setup.

--- src/main.ts
+++ src/main.ts
@@ -13,12 +13,7 @@ export class CustomIoAdapter extends IoAdapter {
     const server = super.createIOServer(port, {
       ...options,
       cors: {
-        origin: [
-          'http://localhost:3001',
-          'http://localhost:3000',
-          'https://rosantibikemotorent.com',
-          'https://api.rosantibikemotorent.com',
-        ],
+        origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
         credentials: true,
         allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
@@ -52,20 +47,11 @@ async function bootstrap() {
         'http://localhost:3001',
         'http://localhost:3000',
         'https://rosantibikemotorent.com',
-        'https://api.rosantibikemotorent.com',
+        '*',
       ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      allowedHeaders: [
-        'Content-Type',
-        'Authorization',
-        'X-Requested-With',
-        'Accept',
-        'Origin',
-        'Access-Control-Allow-Origin',
-      ],
-      exposedHeaders: ['Content-Range', 'X-Content-Range'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
       credentials: true,
-      maxAge: 3600,
     });
 
     // Konfigurasi adapter WebSocket
